7b2bf7156fe4ea68a331490163c7b73c7aa4ceb9
thiagosenac-dev <thiagodosenac@gmail.com>
2026-05-21 19:45:05 -0300

    Add files via upload

---
+++ TESTEDEPROVA/src/main/java/br/com/senac/testedeprova/TestedeprovaApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.testedeprova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestedeprovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestedeprovaApplication.class, args);
+	}
+
+}
---
+++ TESTEDEPROVA/src/test/java/br/com/senac/testedeprova/TestedeprovaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.testedeprova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestedeprovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
